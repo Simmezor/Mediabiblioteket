5a7f8f0a6427447f5bd0fee43210f42b72dab051
ronniegrahn <45630259+ronniegrahn@users.noreply.github.com>
2019-09-20 20:41:28 +0200

    Added searchMediaTitleByString

--- Mediabiblioteket/src/mediabiblioteket/LibraryControllerTest.java
+++ Mediabiblioteket/src/mediabiblioteket/LibraryControllerTest.java
@@ -23,5 +23,28 @@ public class LibraryControllerTest {
 		boolean doesNotExist = libraryController.checkIfBorrowerExist("666");
 		assertEquals(false, doesNotExist);
 	}
+	
+	@Test
+	public void searchMediaTitleByStringTest()
+	{
+		//Arrange
+		LibraryController libraryController = new LibraryController();
+		String theSearchString = "Sin";
+		
+		//Act
+		libraryController.searchMediaTitleByString(theSearchString);
+		
+		//Assert
+		//För att kolla om det funkar ändrade jag om if satsen i searchMediaTitleByString till:
+		
+		//if (tempSearch.getTitle().toLowerCase().contains(theSearchString.toLowerCase()))
+		//{
+		//	mediaSearchResults.add(tempSearch);
+		//	//theGUI.setTheTextArea(tempSearch.toString());
+		//	System.out.print("Test passed!");
+		//}
+		//Glöm inte att ändra tillbaka ;)
+		
+	}
 
 }
