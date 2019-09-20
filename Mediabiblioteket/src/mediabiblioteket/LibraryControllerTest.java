9c57a930844e572bccf07a978628f71b12f983e7
ronniegrahn <45630259+ronniegrahn@users.noreply.github.com>
2019-09-20 21:13:10 +0200

    Added searchMediaTitleByStringTestFail

--- Mediabiblioteket/src/mediabiblioteket/LibraryControllerTest.java
+++ Mediabiblioteket/src/mediabiblioteket/LibraryControllerTest.java
@@ -46,5 +46,30 @@ public class LibraryControllerTest {
 		//Glöm inte att ändra tillbaka ;)
 		
 	}
+	
+	@Test
+	public void searchMediaTitleByStringTestFail()
+	{
+		//Arrange
+		LibraryController libraryController = new LibraryController();
+		String theSearchString = "garrrhh";
+		
+		//Act
+		libraryController.searchMediaTitleByString(theSearchString);
+		
+		//Assert
+		//För att kolla om det funkar ändrade jag om if satsen i searchMediaTitleByString till:
+		
+		//if (tempSearch.getTitle().toLowerCase().contains(theSearchString.toLowerCase()))
+		//{
+		//	mediaSearchResults.add(tempSearch);
+		//	//theGUI.setTheTextArea(tempSearch.toString());
+		//	System.out.print("Test passed!");
+		//}
+		//Glöm inte att ändra tillbaka ;)
+		
+		//I detta fallet failar den och println nås inte. Inget skrivs ut.
+		
+	}
 
 }
